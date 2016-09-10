cd5b789b58b542cad2968126d935d4492704868b
Caden Jiang <jiangshen16@icloud.com>
2016-09-10 02:09:28 -0400

    FB Integration

--- app/src/main/java/airpnp/pennapps/com/airpnp/LoginActivity.java
+++ app/src/main/java/airpnp/pennapps/com/airpnp/LoginActivity.java
@@ -5,16 +5,74 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class LoginActivity extends AppCompatActivity {
+
+    //    Facebook Login
+    private LoginButton btnFBLogin;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        FacebookSdk.sdkInitialize(getApplicationContext());
+//        AppEventsLogger.activateApp(this);
+//        callbackManager = CallbackManager.Factory.create();
+
+//        btnFBLogin = (LoginButton) findViewById(R.id.fb_login_button);
+//        btnFBLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//            @Override
+//            public void onSuccess(LoginResult loginResult) {
+//                AccessToken accessToken = loginResult.getAccessToken();
+//                Profile profile;
+//                ProfileTracker mProfileTracker;
+//                if(Profile.getCurrentProfile() == null) {
+//                    mProfileTracker = new ProfileTracker() {
+//                        @Override
+//                        protected void onCurrentProfileChanged(Profile oldProf, Profile newProf) {
+//                            // profile2 is the new profile
+//                            Log.v("facebook - profile", newProf.getName());
+//                            Profile.setCurrentProfile(newProf);
+//                        }
+//                    };
+//                    mProfileTracker.startTracking();
+//                } else {
+//                    profile = Profile.getCurrentProfile();
+//                    Log.v("facebook - profile", profile.getName());
+//                }
+//                // TODO GET FB DATA FOR NEXT ACTIVITY HERE!!
+////                System.out.println("WTF: "+ " --- "+loginResult.getAccessToken().getUserId() + " Token: " + loginResult.getAccessToken().getToken());
+//            }
+//
+//            @Override
+//            public void onCancel() {
+//                System.out.println("WTF: Canceled");
+//            }
+//
+//            @Override
+//            public void onError(FacebookException e) {
+//                System.out.println("WTF: Error");
+//            }
+//        });
+
         setContentView(R.layout.activity_login);
+
+
 
         final Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/ffs.ttf");
         final TextView tv = (TextView) findViewById(R.id.welcome_text);
