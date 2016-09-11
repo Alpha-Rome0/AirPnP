2d53a817b5fe385d52b61eb05ab45906d4bf8fee
Mete0 <majitsu@gmail.com>
2016-09-11 09:06:39 -0400

    adding review listview

--- app/src/main/java/airpnp/pennapps/com/airpnp/OwnerActivity.java
+++ app/src/main/java/airpnp/pennapps/com/airpnp/OwnerActivity.java
@@ -1,21 +1,38 @@
 package airpnp.pennapps.com.airpnp;
 
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class OwnerActivity extends AppCompatActivity {
 
     TextView ownersText;
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner);
-
+        listview=(ListView)findViewById(R.id.reviews);
         ownersText = (TextView) findViewById(R.id.owner_text);
         final Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/ffs.ttf");
         ownersText.setTypeface(tf);
+
+        ArrayList<String> reviews=new ArrayList();
+        reviews.add("test");
+        reviews.add("test");
+        ListAdapter adapter = new ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_2,
+                reviews);
+
+        // Bind to our new adapter.
+        listview.setAdapter(adapter);
     }
 }
